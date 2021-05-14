d38b87dfa0610a82b53e6e1ac6b87afeccfc205d
Piseth Chhun <smithiesbay@gmail.com>
2021-05-14 17:37:05 +0700

    User Role Permission and fix fontend bug

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -8,19 +8,20 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ trans('panel.site_title') }}</title>
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" />
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" rel="stylesheet" />
-    <link href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" rel="stylesheet" />
-    <link href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css" rel="stylesheet" />
-    <link href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" rel="stylesheet" />
-    <link href="https://cdn.datatables.net/buttons/1.2.4/css/buttons.dataTables.min.css" rel="stylesheet" />
-    <link href="https://cdn.datatables.net/select/1.3.0/css/select.dataTables.min.css" rel="stylesheet" />
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/css/select2.min.css" rel="stylesheet" />
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker.min.css" rel="stylesheet" />
-    <link href="https://unpkg.com/@coreui/coreui@3.2/dist/css/coreui.min.css" rel="stylesheet" />
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" rel="stylesheet" />
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.5.1/min/dropzone.min.css" rel="stylesheet" />
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/jquery.perfect-scrollbar/1.5.0/css/perfect-scrollbar.min.css" rel="stylesheet" />
+    <link href="{{ asset('plugins/css/bootstrap-4.1.3/bootstrap.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('plugins/css/font-awesome-4.7.0/font-awesome.css') }}" rel="stylesheet" />
+    <link href="{{ asset('plugins/css/font-awesome-4.7.0/v5.2.0.all.css') }}" rel="stylesheet" />
+    <link href="{{ asset('plugins/css/datatable-1.10.19/jquery.dataTables.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('plugins/css/datatable-1.10.19/dataTables.bootstrap4.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('plugins/css/datatable-1.10.19/buttons-1.2.4/buttons.dataTables.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('plugins/css/datatable-1.10.19/select-1.3.0/select.dataTables.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('plugins/css/datatable-1.10.19/select2-4.0.5/select2.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('plugins/css/datepicker-4.17.47/bootstrap-datetimepicker.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('plugins/css/dropzone-5.5.1/dropzone.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('plugins/css/perfect-scrollbar-1.5.0/perfect-scrollbar.min.css') }}" rel="stylesheet" />
+
+    {{-- Require CSS --}}
+    <link href="{{ asset('plugins/css/coreui.min.css') }}" rel="stylesheet" />
     <link href="{{ asset('css/custom.css') }}" rel="stylesheet" />
     @yield('styles')
 </head>
@@ -89,27 +90,34 @@
             </form>
         </div>
     </div>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.perfect-scrollbar/1.5.0/perfect-scrollbar.min.js"></script>
-    <script src="https://unpkg.com/@coreui/coreui@3.2/dist/js/coreui.min.js"></script>
-    <script src="//cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
-    <script src="//cdn.datatables.net/buttons/1.2.4/js/dataTables.buttons.min.js"></script>
-    <script src="//cdn.datatables.net/buttons/1.2.4/js/buttons.flash.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/1.2.4/js/buttons.html5.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/1.2.4/js/buttons.print.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/1.2.4/js/buttons.colVis.min.js"></script>
-    <script src="https://cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/pdfmake.min.js"></script>
-    <script src="https://cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/vfs_fonts.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jszip/2.5.0/jszip.min.js"></script>
-    <script src="https://cdn.datatables.net/select/1.3.0/js/dataTables.select.min.js"></script>
-    <script src="https://cdn.ckeditor.com/ckeditor5/16.0.0/classic/ckeditor.js"></script>
+    <script src="{{asset('plugins/js/3.6.0/jquery.min.js')}}"></script>
+    {{-- v4.1.1 --}}
+    <script src="{{asset('plugins/js/bootstrap-4.1.1/bootstrap.min.js')}}"></script> 
+    <script src="{{asset('plugins/js/popper/popper.min.js')}}"></script>
+    <script src="{{asset('plugins/js/perfect-scrollbar-1.5.1/js/perfect-scrollbar.min.js')}}"></script>
+    <script src="{{ asset('plugins/js/@coreui/coreui.min.js') }}"></script>
+    <script src="{{ asset('plugins/js/datatable/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('plugins/js/datatable/dataTables.bootstrap4.min.js') }}"></script>
+
+    {{-- v1.2.4 --}}
+    <script src="{{ asset('plugins/js/datatable/buttons-1.2.4/dataTables.buttons.min.js') }}"></script>
+    <script src="{{ asset('plugins/js/datatable/buttons-1.2.4/buttons.flash.min.js') }}"></script>
+    <script src="{{ asset('plugins/js/datatable/buttons-1.2.4/buttons.html5.min.js') }}"></script>
+    <script src="{{ asset('plugins/js/datatable/buttons-1.2.4/buttons.print.min.js') }}"></script>
+    <script src="{{ asset('plugins/js/datatable/buttons-1.2.4/buttons.colVis.min.js') }}"></script>
+    <script src="{{ asset('plugins/js/datatable/pdfmaker-0.1.18/pdfmake.min.js') }}"></script>
+
+
+    <script src="{{ asset('plugins/js/vfs_fonts.js') }}"></script>
+    <script src="{{ asset('plugins/js/JSZip-2.5.0/jszip.min.js') }}"></script>
+    <script src="{{ asset('plugins/js/datatable/dataTables.select.min.js') }}"></script>
+    <script src="{{ asset('plugins/js/ckeditor/ckeditor.js') }}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/js/select2.full.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.5.1/min/dropzone.min.js"></script>
+    <script src="{{ asset('plugins/js/datepicker-4.17.47/bootstrap-datetimepicker.min.js') }}"></script>
+
+    {{-- v4.0.5 --}}
+    <script src="{{ asset('plugins/js/select2-4.0.5/select2.full.min.js') }}"></script>
+    <script src="{{ asset('plugins/js/dropzone.min.js') }}"></script>
     <script src="{{ asset('js/main.js') }}"></script>
     <script>
         $(function() {
